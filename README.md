8c5821a1d442561ebe15e06c9834ddc3fbbc62ea
Arkadiusz Mula <arekmula97@gmail.com>
2021-02-13 22:31:18 +0100

    Added tensorflow depedency

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ros_handler_detector
+
+## Dependencies
+ROS Melodic
+Tensorflow
+```
+
+```
+
+## Run with
+
+- Setup path to your model directory:
+```
+rosparam set model_dir "path/to/model"
+```
--- scripts/handler_detector.py
+++ scripts/handler_detector.py
@@ -1,8 +1,11 @@
 #!usr/bin/env python
 
+import os
 import sys
+from pathlib import Path
 
 import cv2
+import tensorflow as tf
 
 # ROS imports
 import rospy
@@ -13,11 +16,14 @@ from cv_bridge import CvBridge, CvBridgeError
 
 class Detector:
 
-    def __init__(self):
+    def __init__(self, model_dir):
         self.image_sub = rospy.Subscriber("hz_image_raw", data_class=Image, callback=self.image_callback, queue_size=1,
                                           buff_size=2**24)
         self.bridge = CvBridge()
+        self.model_dir = model_dir
         cv2.namedWindow("Image")
+        self.model = None
+        self.load_model()
 
     def image_callback(self, data):
         try:
@@ -28,10 +34,20 @@ class Detector:
         cv2.imshow("Image", cv_image)
         cv2.waitKey(1)
 
+    def load_model(self):
+        model_dir = os.path.join(Path(self.model_dir), "saved_model")
+        self.model = tf.compat.v2.saved_model.load((str(model_dir)), None)
+        self.model = self.model.signatures["serving_default"]
+
 
 def main(args):
     rospy.init_node("handler_detector")
-    detector = Detector()
+    if rospy.has_param("model_dir"):
+        model_dir = rospy.get_param("model_dir")
+        print(model_dir)
+
+    detector = Detector(model_dir)
+
     try:
         rospy.spin()
     except KeyboardInterrupt:
